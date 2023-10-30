d0600b368da4fa5ae1725f93720535a674d7170a
Borja <pintocorreas@gmail.com>
2023-10-30 17:35:11 +0100

    feat: creation delete work

--- src/controllers/tattooController.ts
+++ src/controllers/tattooController.ts
@@ -1,69 +1,85 @@
-import { Request, Response } from "express"
-import { Tattoo } from "../models/Tattoo"
+import { Request, Response } from "express";
+import { Tattoo } from "../models/Tattoo";
 
-
-const createTattoo = async(req: Request, res: Response) => {
+const createTattoo = async (req: Request, res: Response) => {
   try {
-    const {work, description, price} = req.body
+    const { work, description, price } = req.body;
 
     //validar si hace falta la info
     //tratar si hace falta la info
 
-    const newTattoo = await Tattoo.create(
+    const newTattoo = await Tattoo.create({
+      id: req.token.id,
+      work,
+      description,
+      price,
+    }).save();
+
+    return res.json({
+      success: true,
+      message: "Appointment retrieved",
+      data: newTattoo,
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: "Appointment cant be created",
+      error: error,
+    });
+  }
+};
+
+const updateTattoById = async (req: Request, res: Response) => {
+  try {
+    const { id, work, description, price } = req.body;
+
+    const updateWork = await Tattoo.update(
       {
-        id: req.token.id,
+        id,
+      },
+      {
         work,
         description,
-        price
+        price,
       }
-    ).save()
+    );
 
-    return res.json(
-      {
-        success: true,
-        message: "Appointment retrieved",
-        data: newTattoo
-      }
-    )
-    
+    return res.json({
+      success: true,
+      message: "Work updated",
+      data: updateWork,
+    });
   } catch (error) {
-    return res.json(
-      {
-        success: false,
-        message: "Appointment cant be created",
-        error: error
-      }
-    )
+    return res.json({
+      success: false,
+      message: "Work cant by updated",
+      error: error,
+    });
   }
-}
+};
+const deleteTattoo = async (req: Request, res: Response) => {
+  try {
+    const id = req.body.id;
 
-const updateTattoById = async(req: Request, res: Response) => {
-    try {
-      const {id, work, description, price} = req.body
-  
-      const updateWork = await Tattoo.update(
-        {
-          id
-        },
-        {
-          work,
-          description,
-          price
-        }
-      )
-  
-      return res.json({
-        success: true,
-        message: "Work updated",
-        data: updateWork
-      })
-    } catch (error) {
-      return res.json({
-        success: false,
-        message: "Work cant by updated",
-        error: error
-      })
+    const tattooToRemove = await Tattoo.findOneBy({
+      id,
+    });
+    if (tattooToRemove) {
+      await Tattoo.remove(tattooToRemove);
     }
-  }
 
-export { createTattoo, updateTattoById }
+    return res.json({
+      success: true,
+      message: "Work deleted",
+      data: tattooToRemove,
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: "Work cant by deleted",
+      error: error,
+    });
+  }
+};
+
+export { createTattoo, updateTattoById, deleteTattoo };
--- src/routes/tattoRouters.ts
+++ src/routes/tattoRouters.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth";
 import { isAdmin } from "../middlewares/isAdmin";
-import { createTattoo, updateTattoById } from "../controllers/tattooController";
+import { createTattoo, deleteTattoo, updateTattoById } from "../controllers/tattooController";
 
 
 const router = Router()
 
 router.post('/create', auth, isAdmin, createTattoo)
 router.put("/update", auth, isAdmin, updateTattoById)
+router.delete("/delete", auth, isAdmin, deleteTattoo)
 
 export { router }
